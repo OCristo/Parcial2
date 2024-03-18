d6345b5cba2fc97bf9bbb44fcfe23d9e712e8a27
OC <joseantoniohh03@gmail.com>
2024-03-18 11:33:12 +0100

    Commit final

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -17,7 +17,7 @@ public class SistemaGestionEmpleados {
     // Mezcla la rama de tus iniciales con la rama main donde debe estar el resultado de todo el código modificado pero funcionando exactamente igual que el código original.
     // Publica el resultado en tu Github
     // Modifica el Readme.md de tu repositorio incluyendo tu nombre completo y una captura de pantalla de una vista visual en tu editor de código de las ramas y los commits de tu proyecto. Puedes instalar alguna extensión o herramienta visual que permita esta visualización. Para poder incluir la imagen de la captura en el Readme.md tendrás que subirla también al repositorio.
-    Responde a la tarea con la URL de tu repositorio.
+    // Responde a la tarea con la URL de tu repositorio.
      */
 
     public static void main(String[] args) {
